b1728a53d64d91c9462379ffaa95641cfbd876a0
Nasiba <nasibasharifzoda@gmail.com>
2022-08-19 06:33:24 -0700

    Hello Word is added

---
+++ src/day1/HelloWord.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("good old days");
+
+      // some more work here
+
+
+
+    }
+}
